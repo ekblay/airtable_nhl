a2df2f54552fa1e714a1a094d2ad7dd8c203d8c1
Emmanuel <ekblay@upei.ca>
2021-11-10 18:42:24 -0400

    Trying to fix jquery issue

--- pages/index.js
+++ pages/index.js
@@ -155,24 +155,24 @@ export default function Home() {
         </head>
 
         <body>
-        <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" onClick={() => {
-                setLoadingView(false);
-                setAnalysisView(false);
-                setStartYear("");
-                setEndYear("");
-                setStatsData("");
-                setTeamID("");
-                clearErrors();
+        {/*<nav className="navbar navbar-light bg-light">*/}
+        {/*    <a className="navbar-brand" onClick={() => {*/}
+        {/*        setLoadingView(false);*/}
+        {/*        setAnalysisView(false);*/}
+        {/*        setStartYear("");*/}
+        {/*        setEndYear("");*/}
+        {/*        setStatsData("");*/}
+        {/*        setTeamID("");*/}
+        {/*        clearErrors();*/}
 
-                setFormView(true);
-            }
-            }>
-                <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30"
-                     className="d-inline-block align-top" alt=""/>
-                    NHL Team Analyser
-            </a>
-        </nav>
+        {/*        setFormView(true);*/}
+        {/*    }*/}
+        {/*    }>*/}
+        {/*        <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30"*/}
+        {/*             className="d-inline-block align-top" alt=""/>*/}
+        {/*            NHL Team Analyser*/}
+        {/*    </a>*/}
+        {/*</nav>*/}
 
 
         {formView  &&
